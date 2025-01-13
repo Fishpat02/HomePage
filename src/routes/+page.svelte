bebd0a04926cedcf04cbdaef60d0e613fdcea545
Fishpat02 <55009589+Fishpat02@users.noreply.github.com>
2025-01-13 09:57:08 -0500

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { page } from '$app/state'
+
+  page.data.title = 'Home'
+</script>
+
+<svelte:head>
+  <title>{page.data.title}</title>
+</svelte:head>
+
+<h1>Welcome to my home page!</h1>
