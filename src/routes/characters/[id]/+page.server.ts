f9a37c501752bb3b7171413e94f04b26f1292492
Fishpat02 <55009589+Fishpat02@users.noreply.github.com>
2025-01-18 14:28:13 -0500

    Updated gitignore

---
+++ src/routes/characters/[id]/+page.server.ts
@@ -0,0 +1,14 @@
+import { characters } from '$lib/db'
+import { error } from '@sveltejs/kit'
+import type { PageServerLoad } from './$types'
+
+export const load: PageServerLoad = async ({ params }) => {
+  const id = parseInt(params.id)
+  const character = characters.find((c) => c.id === id)
+
+  if (!character) {
+    error(404, {
+      message: 'Not found',
+    })
+  }
+}
