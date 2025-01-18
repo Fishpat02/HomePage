56be4e00d392fceb2e1ab6a2b0ca14bed95ac84b
Fishpat02 <55009589+Fishpat02@users.noreply.github.com>
2025-01-18 14:50:51 -0500

    Ran deno fmt

--- src/routes/characters/+page.svelte
+++ src/routes/characters/+page.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
-    import { characters } from '$lib/db'
+  import { characters } from '$lib/db'
 </script>
 
 <ul class="list-none">
-{#each characters as character}
-    <li class="text-xl"><a href="/characters/{character.id}">{character.name}</a></li>
-{/each}
+  {#each characters as character}
+    <li class="text-xl">
+      <a href="/characters/{character.id}">{character.name}</a>
+    </li>
+  {/each}
 </ul>
