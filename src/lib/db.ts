4c8716b796c78048809ea1c8a00c08f4b99e3847
Fishpat02 <55009589+Fishpat02@users.noreply.github.com>
2025-01-18 21:19:59 -0500

    Added new DB implementation to db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,52 +1,21 @@
-export const characters: Character[] = [
-  {
-    id: 1,
-    name: 'John Doe',
-    age: 30,
-    gender: 'Male',
-    species: 'Human',
-    shortDescription: 'A mysterious wanderer.',
-    longDescription:
-      'John Doe is a mysterious wanderer with a troubled past. He travels from town to town, helping those in need.',
-    appearance: {
-      eyeColor: 'blue',
-      hairColor: 'brown',
-      height: 180,
-      weight: 75,
-    },
-  },
-  {
-    id: 2,
-    name: 'Jane Smith',
-    age: 28,
-    gender: 'Female',
-    species: 'Elf',
-    shortDescription: 'A skilled archer.',
-    longDescription:
-      'Jane Smith is an elf with exceptional archery skills. She is known for her sharp eyes and quick reflexes.',
-    appearance: {
-      eyeColor: 'green',
-      hairColor: 'blonde',
-      height: 170,
-      weight: 60,
-      tattoos: 'A small leaf on her wrist',
-    },
-  },
-  {
-    id: 3,
-    name: 'Gorak',
-    age: 40,
-    gender: 'Male',
-    species: 'Orc',
-    shortDescription: 'A fierce warrior.',
-    longDescription:
-      'Gorak is a fierce orc warrior with unmatched strength. He has a scarred face and a muscular build.',
-    appearance: {
-      eyeColor: 'red',
-      hairColor: 'black',
-      height: 200,
-      weight: 120,
-      marks: 'A large scar across his face',
-    },
-  },
-]
+import { MongoClient } from 'mongodb'
+
+const DATABASE = 'HomePage'
+const COLLECTION = 'Characters'
+
+const uri =
+  `mongodb://${import.meta.env.VITE_MONGO_USER}:${import.meta.env.VITE_MONGO_PW}@localhost:27017/`
+const client = new MongoClient(uri)
+
+async function addCharacter(character: Character) {
+  try {
+    await client.connect()
+
+    const db = client.db(DATABASE)
+    const coll = db.collection(COLLECTION)
+
+    await coll.insertOne(character)
+  } finally {
+    await client.close()
+  }
+}
