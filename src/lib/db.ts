e1827daaf87351b184e77fcc42bebbb59073a6f5
Fishpat02 <55009589+Fishpat02@users.noreply.github.com>
2025-01-18 21:35:46 -0500

    Added getCharacterCount function to db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -7,7 +7,20 @@ const uri =
   `mongodb://${import.meta.env.VITE_MONGO_USER}:${import.meta.env.VITE_MONGO_PW}@localhost:27017/`
 const client = new MongoClient(uri)
 
-async function addCharacters(characters: Character[]) {
+export async function getCharacterCount() {
+  try {
+    await client.connect()
+
+    const db = client.db(DATABASE)
+    const coll = db.collection(COLLECTION)
+
+    return await coll.countDocuments()
+  } finally {
+    await client.close()
+  }
+}
+
+export async function addCharacters(characters: Character[]) {
   try {
     await client.connect()
 
