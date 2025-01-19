c634207cf17f124fe89d1083925afb19edc416b3
Fishpat02 <55009589+Fishpat02@users.noreply.github.com>
2025-01-18 21:21:55 -0500

    Changed addCharacter to take an array

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -7,14 +7,14 @@ const uri =
   `mongodb://${import.meta.env.VITE_MONGO_USER}:${import.meta.env.VITE_MONGO_PW}@localhost:27017/`
 const client = new MongoClient(uri)
 
-async function addCharacter(character: Character) {
+async function addCharacters(characters: Character[]) {
   try {
     await client.connect()
 
     const db = client.db(DATABASE)
     const coll = db.collection(COLLECTION)
 
-    await coll.insertOne(character)
+    await coll.insertMany(characters)
   } finally {
     await client.close()
   }
