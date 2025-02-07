3083254d9b454cebd1d4ed4a6295995f9f04d3fd
Fishpat02 <55009589+Fishpat02@users.noreply.github.com>
2025-02-07 14:31:24 -0500

    Added mode-switcher for light/dark theming

--- src/lib/components/app-sidebar.svelte
+++ src/lib/components/app-sidebar.svelte
@@ -3,6 +3,11 @@
   import House from 'lucide-svelte/icons/house'
   import Social from 'lucide-svelte/icons/message-circle-question'
   import * as Sidebar from '$lib/components/ui/sidebar'
+  import Sun from 'lucide-svelte/icons/sun'
+  import Moon from 'lucide-svelte/icons/Moon'
+  import {Button} from '$lib/components/ui/button'
+
+  import { toggleMode } from 'mode-watcher'
 
   const items = [
     {
@@ -45,4 +50,11 @@
       </Sidebar.GroupContent>
     </Sidebar.Group>
   </Sidebar.Content>
+  <Sidebar.Footer>
+    <Button onclick={toggleMode} variant="outline" size="icon">
+      <Sun class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <Moon class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <span class="sr-only">Toggle theme</span>
+    </Button>
+  </Sidebar.Footer>
 </Sidebar.Root>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
   import * as Sidebar from '$lib/components/ui/sidebar'
   import AppSidebar from '$lib/components/app-sidebar.svelte';
+  import { ModeWatcher } from 'mode-watcher'
 
   import '../app.css'
   import type { SvelteHTMLElements } from 'svelte/elements'
+
 
   let { children }: SvelteHTMLElements['div'] = $props()
 </script>
@@ -17,6 +19,7 @@
   <AppSidebar />
 
   <main>
+    <ModeWatcher />
     {@render children?.()}
   </main>
 </Sidebar.Provider>
